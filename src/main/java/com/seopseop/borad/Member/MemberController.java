faea19eda6b229306fdd785bd047c0ff031196ad
gimgangseob <rlarkdtjq590@naver.com>
2024-06-06 15:49:49 +0900

    member_update

--- src/main/java/com/seopseop/borad/Member/MemberController.java
+++ src/main/java/com/seopseop/borad/Member/MemberController.java
@@ -8,6 +8,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -53,26 +54,72 @@ public class MemberController {
     }
 
     @GetMapping("/infochange")
-    public String GetInfoChange () {
+    public String GetInfoChange (Authentication auth) {
+        if (auth == null || !auth.isAuthenticated()) {
+            return "redirect:/user/login";
+        }
         return "infochange.html";
     }
     @PostMapping("/infochange")
-    public String PostInfoChange () {
+    public String PostInfoChange (@RequestParam String nickname,
+                                  Authentication auth) {
+        Optional<Member> result = memberRepository.findByUsername(auth.getName());
+
+        if (result.isEmpty()) {
+            return "redirect:/user/login";
+        }
+        var user = result.get();
+        user.setId(user.getId());
+        user.setUsername(user.getUsername());
+        user.setPassword(user.getPassword());
+        user.setNickname(nickname);
+        memberRepository.save(user);
+
         return "redirect:/list/1";
     }
 
     @GetMapping("/passwordchange")
-    public String GetPasswordChange () {
+    public String GetPasswordChange (Authentication auth) {
+        if (auth == null || !auth.isAuthenticated()) {
+            return "redirect:/user/login";
+        }
         return "passwordchange.html";
     }
 
     @PostMapping("/passwordchange")
-    public String PostPasswordChange () {
-        return "redirect:/list/1";
+    public String PostPasswordChange (@RequestParam String password1,
+                                      @RequestParam String password2,
+                                      Authentication auth) {
+
+        Optional<Member> result = memberRepository.findByUsername(auth.getName());
+
+        if (result.isEmpty()) {
+            return "redirect:/user/login";
+        }
+
+        if (password1.equals(password2) && password1.length() > 7) {
+            var user = result.get();
+            var hashed = new BCryptPasswordEncoder().encode(password1);
+            user.setId(user.getId());
+            user.setUsername(user.getUsername());
+            user.setNickname(user.getNickname());
+            user.setPassword(hashed);
+            return "redirect:/list/1";
+        } else {
+            return "redirect:/user/passwordchange?error=password_has_problem";
+        }
     }
 
     @GetMapping("/mypage")
-    public String mypage (Authentication auth) {
+    public String mypage (Authentication auth,
+                          Model model) {
+
+        if (auth == null || !auth.isAuthenticated()) {
+            return "redirect:/user/login";
+        }
+        Optional<Member> result = memberRepository.findByUsername(auth.getName());
+        var user = result.get();
+        model.addAttribute("user",user);
         return "mypage.html";
     }
 
