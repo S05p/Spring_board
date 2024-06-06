52bd594e20b61820c307491d3a10e9eff2eb52a2
gimgangseob <rlarkdtjq590@naver.com>
2024-06-06 16:18:18 +0900

    readme

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Spring_board
+
+* Post CRUD 구현
+> pagination 구현
+* User CRUD 구현
--- src/main/java/com/seopseop/borad/Member/MemberController.java
+++ src/main/java/com/seopseop/borad/Member/MemberController.java
@@ -1,13 +1,8 @@
 package com.seopseop.borad.Member;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import org.springframework.beans.factory.parsing.PassThroughSourceExtractor;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
